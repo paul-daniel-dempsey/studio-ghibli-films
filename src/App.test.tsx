8d4784fedb5e1aee3a29770cd25db28eda97ee67
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-04-28 19:52:33 +0100

    Error 500 418 checks, npm test passes, npm start fails (404)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,27 @@
 
 import {setupServer} from 'msw/node'
 
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import { rest } from 'msw';
 
 const server = setupServer(
   rest.get('https://ghibliapi.herokuapp.com/films', (req, res, ctx) => {
-    return res(ctx.json([{title: 'Castle Sky',description:'The orphan Sheeta...',image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg'}]))
+  return res(ctx.json([{title: 'Castle1 Sky',description:'The orphan Sheeta...',image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg'}]))
+  }),
+  rest.get('https://ghibliapi.herokuapp.com/err500', (req, res, ctx) => {
+    return res( ctx.status(500,'Oops… something went wrong, try again 🤕'),
+                ctx.json([{}]))
+  }),
+  rest.get('https://ghibliapi.herokuapp.com/err418', (req, res, ctx) => {
+    return res( ctx.status(418,"I'm a tea postMessage 🫖, silly"),
+                ctx.json([{}]))
   }),
 )
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
-
 
 
 test('renders learn react link', () => {
@@ -26,8 +33,26 @@ test('renders learn react link', () => {
 test('Checks Mock API has interjected', async () => {
   render(<App />);
 
-  await screen.findByTestId('CastleSky')
+  await screen.findByTestId('Castle1Sky')
 
-  const linkElement = screen.getByTestId('CastleSky');
+  const linkElement = screen.getByTestId('Castle1Sky');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Checks Mock API Error500 has interjected', async () => {
+  render(<App />);
+
+  await screen.findByTestId('500')
+
+  const linkElement = screen.getByTestId('500');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Checks Mock API Error418 has interjected', async () => {
+  render(<App />);
+
+  await screen.findByTestId('418')
+
+  const linkElement = screen.getByTestId('418');
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { filmObj } from './components/film';
 import Films from './components/films';
 import Header from './components/header';
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 function App() {
 
@@ -15,8 +15,22 @@ function App() {
   }, [currentPage])
 
   const getFilms = async (pageNumber: number) => {
-    const apiResponse = await axios.get(`https://ghibliapi.herokuapp.com/films?page=${pageNumber}`);
-    setFilms(apiResponse.data);
+
+      //await axios.get(`https://ghibliapi.herokuapp.com/err500?page=${pageNumber}`)
+      //await axios.get(`https://ghibliapi.herokuapp.com/err418?page=${pageNumber}`)
+      await axios.get(`https://ghibliapi.herokuapp.com/films?page=${pageNumber}`)
+      .then((apiResponse: AxiosResponse) => {
+        setFilms(apiResponse.data);
+      })
+      .catch((reason: AxiosError) => {
+        if(reason.response !== undefined) {
+          // Will not display this message only Error 404 via 'npm start'
+          // 'npm test' -> HTML contains correct error code, associated App.test.tsx passes
+          // 'npm start' -> 'Fail to load resource' 404 when reading 'reason.response.status/statusTest'?
+          console.log(`${reason.response.status} : ${reason.response.statusText}`);
+          setFilms([{_id:1, title: reason.response.status.toString(), description: reason.response.statusText, image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg'}]);
+        }
+      })
   };
 
   return (
